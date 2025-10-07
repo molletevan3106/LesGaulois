00db0a9d5e3ca40d60a844b93626eb5cf6890b9a
molletevan3106 <mollet.evan3106@gmail.com>
2025-10-07 16:09:43 +0200

    fin du tp2 avec les enums

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,12 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbequipement=0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -38,8 +42,42 @@ public class Romain {
 	private boolean isInvariantVerified() {
 		return force>=0;
 	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (nbequipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+			} else {
+				ajouterEquipement(equipement);
+			}
+			break;
+		
+		case 0:
+			ajouterEquipement(equipement);
+			break;
+			
+		}
+	}
+		
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbequipement]=equipement;
+		nbequipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+		
+	}
+
 	public static void main(String[] args) {
 		Romain minus= new Romain("Minus",6);
 		System.out.println(minus.isInvariantVerified());
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
